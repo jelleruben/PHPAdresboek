7acd6af335abcd7f45cca99ce73a196a2761b649
Jelle Ruben <jelleruben@gmail.com>
2020-05-26 18:31:24 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database variablen. */
-define('DB_SERVER', '192.168.178.13');
-define('DB_USERNAME', 'jelleruben');
-define('DB_PASSWORD', '@11AuGu78!');
-define('DB_NAME', 'adresboek');
-define('DB_PORT', 3307);
+define('DB_SERVER', '*******');
+define('DB_USERNAME', '**********');
+define('DB_PASSWORD', '*********');
+define('DB_NAME', '**********');
+define('DB_PORT', ******);
  
 /* Connectie naar Database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
